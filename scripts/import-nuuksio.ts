72d8c166fe1320f92d84be1698d3991d3af26bf0
Tiina Siremaa <tiinatuuliak@gmail.com>
2026-05-28 14:44:29 +0300

    datapipeline toimii ja testattu

--- scripts/import-nuuksio.ts
+++ scripts/import-nuuksio.ts
@@ -1,4 +1,8 @@
 import fs from 'fs';
+import { Client } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const rawData = fs.readFileSync(
   './data/nuuksio.geojson',
@@ -7,15 +11,57 @@ const rawData = fs.readFileSync(
 
 const geojson = JSON.parse(rawData);
 
-console.log(geojson);
-
-geojson.features.forEach((feature: any) => {
-  console.log('Nimi:', feature.properties.name);
-  console.log('Tyyppi:', feature.properties.type);
-  console.log(
-    'Koordinaatit:',
-    feature.geometry.coordinates
-  );
-
-  console.log('---');
+const client = new Client({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+async function importLocations() {
+  await client.connect();
+
+  for (const feature of geojson.features) {
+    const name = feature.properties.name;
+    const type = feature.properties.type;
+    const description =
+      feature.properties.description;
+
+    const longitude =
+      feature.geometry.coordinates[0];
+
+    const latitude =
+      feature.geometry.coordinates[1];
+
+   await client.query(
+  `
+  INSERT INTO locations
+  (name, type, description, latitude, longitude, geom)
+  VALUES (
+    $1,
+    $2,
+    $3,
+    $4,
+    $5,
+    ST_SetSRID(ST_MakePoint($5, $4), 4326)
+  )
+  `,
+  [
+    name,
+    type,
+    description,
+    latitude,
+    longitude,
+  ]
+);
+
+    console.log('Lisätty:', name);
+  }
+
+  await client.end();
+
+  console.log('Import valmis');
+}
+
+importLocations();
